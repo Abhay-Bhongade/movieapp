7711b5f0a97196eb2054cb26838ae7b1706f62d2
Abhay Bhongade <abhaybhongade63@gmail.com>
2022-04-06 00:31:12 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Powered by Movieshub</div>
+      <div>©2022, All Rights Reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./PageNotFound.scss"
+
+const PageNotFound = () => {
+  return (
+    <div className="pagenotfound">PageNotFound</div>
+  );
+};
+
+export default PageNotFound;
